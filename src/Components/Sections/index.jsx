00203df6f96ea87a3b052b604cba122a323dfa34
Abu khaled <mkhkatanany@gmail.com>
2023-12-26 19:33:53 +0200

    make the privete route just the cart

--- src/Components/Sections/index.jsx
+++ src/Components/Sections/index.jsx
@@ -1,31 +1,31 @@
-import { Link } from 'react-router-dom'
-import useProducts from '../../Hook/useProducts'
-import { Message, MyCart, Orders, Profile } from '../../Icons'
-import { SectionsStyle } from './Styled'
+import { Link } from "react-router-dom";
+import useProducts from "../../Hook/useProducts";
+import { Message, MyCart, Orders, Profile } from "../../Icons";
+import { SectionsStyle } from "./Styled";
 
 const Sections = () => {
-  const { total} = useProducts()
+  const { total } = useProducts();
 
   return (
-    <SectionsStyle productLength = {total}>
-      <Link to="/Home/" className="icon">
+    <SectionsStyle productLength={total}>
+      <Link to="/home" className="icon">
         <Profile />
         <p>Profile</p>
       </Link>
-      <Link to="/Home/page2" className="icon">
+      <Link to="/page2" className="icon">
         <Message />
         <p>Message</p>
       </Link>
-      <Link to="/Home/page3" className="icon">
+      <Link to="/page3" className="icon">
         <Orders />
         <p>Orders</p>
-      </Link >
-      <Link to="/Home/page4" className="icon">
+      </Link>
+      <Link to="/page4" className="icon">
         <MyCart />
         <p>My cart</p>
-      </Link >
+      </Link>
     </SectionsStyle>
-  )
-}
+  );
+};
 
-export default Sections
+export default Sections;
--- src/Routes.js
+++ src/Routes.js
@@ -1,33 +1,39 @@
-import { lazy, Suspense, useContext } from "react"
-import { Route, Routes, Navigate } from "react-router-dom"
-import PageNotFound from "./Components/PageNotFound"
-import { AuthContext } from "./Context"
-import Loading from "./Components/Loading"
-const Home = lazy(()=>import("./Pages/Home"))
-const Page2 = lazy(()=>import("./Pages/Page2"))
-const Page3 = lazy(()=>import("./Pages/Page3"))
-const Page4 = lazy(()=>import("./Pages/Page4"))
-const Register = lazy(()=>import("./Pages/Register"))
-const SignIn = lazy(()=>import("./Pages/SignIn"))
-const PrivateRoute = lazy(()=>import("./PriveteRoutes"))
-const MyRoutes = ()=>{
-    const [ isAuthenticated,  ] = useContext(AuthContext);
-    return (
-      <Suspense fallback={<Loading/>}>
-        <Routes>
-          <Route index element={<Navigate to='/login' />} />
-          <Route path="/login" element={isAuthenticated?<Navigate to='/Home' />:<SignIn/>} />
-          <Route path="/register" element={isAuthenticated?<Navigate to='/Home' />:<Register />} />
-          <Route path="/Home" element={<PrivateRoute/>}>
-            <Route index element={<Home />} />
-            <Route path="page2" element={<Page2 />} />
-            <Route path="page3" element={<Page3 />} />
-            <Route path="page4" element={<Page4 />} />
-            <Route path="*" element={<PageNotFound/>} />
-          </Route>
-            <Route path="/*" element={<PageNotFound/>} />
-        </Routes>
-      </Suspense>
-    )
-}
+import { lazy, Suspense, useContext } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import PageNotFound from "./Components/PageNotFound";
+import { AuthContext } from "./Context";
+import Loading from "./Components/Loading";
+const Home = lazy(() => import("./Pages/Home"));
+const Page2 = lazy(() => import("./Pages/Page2"));
+const Page3 = lazy(() => import("./Pages/Page3"));
+const Page4 = lazy(() => import("./Pages/Page4"));
+const Register = lazy(() => import("./Pages/Register"));
+const SignIn = lazy(() => import("./Pages/SignIn"));
+const PrivateRoute = lazy(() => import("./PriveteRoutes"));
+const MyRoutes = () => {
+  const [isAuthenticated] = useContext(AuthContext);
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route index element={<Navigate to="/home" />} />
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/home" /> : <SignIn />}
+        />
+        <Route
+          path="/register"
+          element={isAuthenticated ? <Navigate to="/home" /> : <Register />}
+        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<PrivateRoute />}>
+          <Route index element={<Page4 />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </Suspense>
+  );
+};
 export default MyRoutes;
