cb779e26705b7e2bdbe890b9559c85ec7d024eca
Abu khaled <mkhkatanany@gmail.com>
2025-03-21 21:46:14 +0200

    make the user authenticate as defoult

--- src/Context/index.js
+++ src/Context/index.js
@@ -5,6 +5,6 @@ import { initialState } from "../productReducer";
 export const ThemeContext = createContext(null);
 export const ProductsDataContext = createContext(initialState);
 export const AuthContext = createContext({
-  isAuthenticated: false,
+  isAuthenticated: true,
   setIsAuthenticated: () => {}
 });
